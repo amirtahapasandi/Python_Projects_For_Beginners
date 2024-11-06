67998edf75b388506bc2954a6df6363e4a6dc786
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2024-11-06 13:03:57 +0330

    make a dice game!

---
+++ dice_game.py
@@ -0,0 +1,14 @@
+import random
+
+while True:
+    first_dice = random.randint(1,6)
+    second_dice = random.randint(1,6)
+    user_choice = input("Roll the dice? (y/n): ")
+    if user_choice == "y":
+        print(f"({first_dice}, {second_dice})")
+        continue
+    elif user_choice == "n":
+        print("Thanks for playing!")
+        break
+    else:
+        print("Invalid choice!")
